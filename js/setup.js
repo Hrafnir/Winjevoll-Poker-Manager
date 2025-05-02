12c6f1e3f3a6328c80dc3ff9e51bb29e383cc707
Hrafnir <eirik.gaarde@gmail.com>
2025-05-02 10:48:34 +0200

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -8,7 +8,8 @@ import {
     saveTemplate,
     generateUniqueId,
     saveTournamentState,
-    setActiveTournamentId
+    setActiveTournamentId,
+    getActiveTournamentId // FIKS: Lagt til denne
 } from './storage.js';
 
 // NYTT: Importer logActivity
